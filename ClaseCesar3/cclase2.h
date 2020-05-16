26efba3fd2633326d875d0670cd209863540ea85
Rupisecter <56146083+Rupisecter@users.noreply.github.com>
2020-05-15 22:12:06 -0500

    Add files via upload

---
+++ ClaseCesar3/cclase2.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Cesar
+{
+  private:
+  int clave, ind;
+  string alfabeto;
+  public:
+  Cesar();
+  string cifra(string msg);
+  string descifrar(string cmsg);
+};
+Cesar::Cesar()
+{
+  alfabeto="abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  clave=3;
+}
+string Cesar::cifra(string msg)
+{
+  int tam=alfabeto.length();
+  int ax;
+  for(int i=0;i<msg.length();i++)
+  {
+    ind=alfabeto.find(msg[i]);
+    ax=ind+clave;
+    msg[i]=alfabeto[(ax)%tam];
+  }
+  return msg;
+}
+string Cesar::descifrar(string cmsg)
+{
+  int tam=alfabeto.length();
+  int ax;
+  for(int i=0;i<cmsg.length();i++)
+  {
+    ind=alfabeto.find(cmsg[i]);
+    ax=ind-clave;
+    cmsg[i]=alfabeto[(ax)%tam];
+  }
+  return cmsg;
+}
